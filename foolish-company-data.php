7b10e741e349b28f2449cdc0f9a1b8eb6a6a3043
vagrant <vagrant@foolish-project.vagrant>
2021-04-21 15:00:50 +0000

    Initial setup

---
+++ foolish-company-data.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Foolish Company Data
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     foolish-company-data
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Foolish_Company_Data
+ */
+
+// Your code starts here.
